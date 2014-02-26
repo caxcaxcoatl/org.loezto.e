7ed0f767949454d829b7f26e02b6560fc7edd109
Danilo Hashimoto <danilo.hashimoto@gmail.com>
2014-02-25 22:36:41 -0300

    Added line column to searchEntryPart

--- org.loezto.e/src/org/loezto/e/part/SearchEntryPart.java
+++ org.loezto.e/src/org/loezto/e/part/SearchEntryPart.java
@@ -1,7 +1,9 @@
 package org.loezto.e.part;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.Calendar;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
@@ -15,9 +17,11 @@ import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TableViewerColumn;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
@@ -25,6 +29,7 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Spinner;
 import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.swt.widgets.Text;
 import org.loezto.e.model.EService;
 import org.loezto.e.model.Entry;
@@ -41,6 +46,8 @@ public class SearchEntryPart {
 
 	@Inject
 	EService eService;
+	private TableViewerColumn vClnDate;
+	private TableViewerColumn vClnLine;
 
 	public SearchEntryPart() {
 	}
@@ -76,7 +83,7 @@ public class SearchEntryPart {
 				ArrayList<Topic> list = new ArrayList<>();
 				list.add((Topic) eContext.get("E_CURRENT_TOPIC"));
 				wl.clear();
-				Date date = null;
+				// Date date = null;
 				wl.addAll(eService.searchEntries(text.getText(), null, null));
 				// wl.addAll(eService.searchEntries(text.getText(), date,
 				// list));
@@ -88,6 +95,24 @@ public class SearchEntryPart {
 		tableViewer = new TableViewer(parent, SWT.BORDER | SWT.FULL_SELECTION);
 		table = tableViewer.getTable();
 		table.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+
+		table.setLinesVisible(true);
+		table.setHeaderVisible(true);
+
+		vClnDate = new TableViewerColumn(tableViewer, SWT.NONE);
+		TableColumn tblclmnDate = vClnDate.getColumn();
+		Calendar today = Calendar.getInstance();
+		String fullDate = SimpleDateFormat.getDateTimeInstance(DateFormat.FULL,
+				DateFormat.FULL).format(today.getTime());
+		GC gc = new GC(table);
+		tblclmnDate.setWidth(gc.textExtent(fullDate).x);
+		tblclmnDate.setText("Date");
+
+		vClnLine = new TableViewerColumn(tableViewer, SWT.NONE);
+		TableColumn tblclmnLine = vClnLine.getColumn();
+		tblclmnLine.setWidth(100);
+		tblclmnLine.setText("Line");
+
 		viewerSetup();
 
 	}
